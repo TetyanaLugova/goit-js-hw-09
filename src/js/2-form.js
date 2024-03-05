3c3339b80b4485a62ca8eafcbc45e8d17adb2c7d
TetyanaLugova <lugovatanya1503@gmail.com>
2024-03-05 17:38:24 +0200

    HW9(finish)

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,12 +1,30 @@
 const formRef = document.querySelector(".feedback-form");
 const emailRef = document.querySelector(".email");
 const messageRef = document.querySelector(".text");
+const localStorageKey = "feedback-form-state";
 
-form.addEventListener("input", function (event) {
+const savedValue = JSON.parse(localStorage.getItem(localStorageKey)) ?? {};
+emailRef.value = savedValue.email ?? "";
+messageRef.value = savedValue.message ?? "";
+
+formRef.addEventListener("input", function (event) {
+    if (event.target.matches('.email') || event.target.matches('.text')) {
+        const formValue = {
+            email: emailRef.value,
+            message: messageRef.value,
+        }
+        localStorage.setItem(localStorageKey, JSON.stringify(formValue));
+    }
+});
+
+
+formRef.addEventListener("submit", function (event) {
+    event.preventDefault();
     const formValue = {
         email: emailRef.value,
         message: messageRef.value,
-   }
-localStorage.setItem("feedback-form-state", JSON.stringify(formValue));
-    
-})
+    };
+    console.log(formValue);
+    localStorage.removeItem(localStorageKey);
+    formRef.reset();
+   } )
