35fa88e3e50deb3ab4484c68602b9348fa134279
TetyanaLugova <148479156+TetyanaLugova@users.noreply.github.com>
2024-03-05 13:10:47 +0000

    Deploying to gh-pages from @ TetyanaLugova/goit-js-hw-09@719c64eaf96354edc91a6dfee2e828b41f9d6f8c 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";document.querySelector(".feedback-form");const t=document.querySelector(".email"),o=document.querySelector(".text");form.addEventListener("input",function(a){const e={email:t.value,message:o.value};localStorage.setItem("feedback-form-state",JSON.stringify(e))});
+//# sourceMappingURL=commonHelpers2.js.map
